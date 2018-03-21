f7daefdfdef10de65441da2172313c1ff9844a07
hanweidonger <13363135267@163.com>
2018-03-21 09:00:43 +0800

    前台页面修改

--- routes/web.php
+++ routes/web.php
@@ -16,32 +16,35 @@ Route::get('/', function () {
 });
 
 Route::group([],function(){
-	
-	Route::get('/lists','homes\HomesController@lists');
-	
-	Route::get('/orders','homes\HomesController@orders');
 
-	Route::get('/center','homes\HomesController@center');
-
-	Route::get('/shop','homes\HomesController@shop');
-
-	Route::get('/safety','homes\HomesController@safety');
-
-    Route::get('/add','homes\HomesController@add');
-
-    Route::get('/password','homes\HomesController@password');
-
-    Route::get('/data','homes\HomesController@data');
-
-    Route::get('/integral','homes\HomesController@integral');
-
-    Route::get('/collect','homes\HomesController@collect');
-
-    Route::get('/balance','homes\HomesController@balance');
-
-    Route::get('/jiesuan','homes\HomesController@jiesuan');
-
-    Route::get('/join','homes\HomesController@join');
-
-    Route::get('/shangjiazhuru','homes\HomesController@shangjiazhuru');
+    //个人中心
+    Route::get('/center','Home\Users\UserController@center');
+    //安全中心
+	Route::get('/safety','Home\Users\UserController@safety');
+    //用户地址
+    Route::get('/add','Home\Users\UserController@add');
+    //用户密码
+    Route::get('/password','Home\Users\UserController@password');
+    //用户数据资料
+    Route::get('/data','Home\Users\UserController@data');
+    //用户积分
+    Route::get('/integral','Home\Users\UserController@integral');
+    //用户收藏
+    Route::get('/collect','Home\Users\UserController@collect');
+    //账户余额
+    Route::get('/balance','Home\Users\UserController@balance');
+    //加盟合作
+    Route::get('/join','Home\Users\UserController@join');
+    //登录
+    Route::get('/login','Home\Logins\LoginController@login');
+    //注册
+    Route::get('/register','Home\Logins\LoginController@register');
+    //商品详情
+    Route::get('/shop','Home\Shops\ShopController@shop');
+    //商品列表
+    Route::get('/lists','Home\Shops\ShopController@lists');
+    //商品订单
+    Route::get('/orders','Home\Orders\OrderController@orders');
+    //结算
+    Route::get('/jiesuan','Home\Orders\OrderController@jiesuan');
 });
