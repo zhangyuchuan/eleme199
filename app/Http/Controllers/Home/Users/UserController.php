7e43f13c30a7551569ce39383b5ccec91a0a4e7e
hanweidonger <13363135267@163.com>
2018-03-22 10:25:28 +0800

    123

---
+++ app/Http/Controllers/Home/Users/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Home\Users;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    //个人中心
+    public function center()
+    {
+        return view('Homes.Users.center');
+    }
+
+    //安全中心
+    public function safety()
+    {
+        return view('Homes.Users.safety');
+    }
+
+    //用户地址
+    public function add()
+    {
+        return view('Homes.Users.add');
+    }
+
+    //用户密码
+    public function password()
+    {
+        return view('Homes.Users.password');
+    }
+
+    //用户数据资料
+    public function data()
+    {
+        return view('Homes.Users.data');
+    }
+
+    //用户积分
+    public function integral()
+    {
+        return view('Homes.Users.integral');
+    }
+
+    //用户收藏
+    public function collect()
+    {
+        return view('Homes.Users.collect');
+    }
+
+    //账户余额
+    public function balance()
+    {
+        return view('Homes.Users.balance');
+    }
+
+    //加盟合作
+    public function join()
+    {
+        return view('Homes.Users.join');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('homes.shouye');
+});
+
+
+
+
+Route::group([],function(){
+
+
+    //后台管理员路由
+    //Admin\Users\MangerController
+    Route::get('/admin/users/manger/index','Admin\Users\MangerController@index');
+    Route::get('/admin/users/manger/list','Admin\Users\MangerController@list');
+
+    //后台普通用户路由
+//    Admin\Users\UserController
+    //后台商家路由
+    //Admin\Users\SellerController
+    //后台商铺路由
+    //Admin\Shops\ShopController
+    //后台商品路由
+    //Admin\Goods\GoodController
+    //后台订单路由
+    //Admin\Orders\OrderController
+});
+
+
+    //个人中心
+    Route::get('/center','Home\Users\UserController@center');
+    //安全中心
+	Route::get('/safety','Home\Users\UserController@safety');
+    //用户地址
+    Route::get('/add','Home\Users\UserController@add');
+    //用户密码
+    Route::get('/password','Home\Users\UserController@password');
+    //用户数据资料
+    Route::get('/data','Home\Users\UserController@data');
+    //用户积分
+    Route::get('/integral','Home\Users\UserController@integral');
+    //用户收藏
+    Route::get('/collect','Home\Users\UserController@collect');
+    //账户余额
+    Route::get('/balance','Home\Users\UserController@balance');
+    //加盟合作
+    Route::get('/join','Home\Users\UserController@join');
+    //登录
+    Route::get('/login','Home\Logins\LoginController@login');
+    //注册
+    Route::get('/register','Home\Logins\LoginController@register');
+    //商品详情
+    Route::get('/shop','Home\Shops\ShopController@shop');
+    //商品列表
+    Route::get('/lists','Home\Shops\ShopController@lists');
+    //商品订单
+    Route::get('/orders','Home\Orders\OrderController@orders');
+    //结算
+    Route::get('/jiesuan','Home\Orders\OrderController@jiesuan');
+    
