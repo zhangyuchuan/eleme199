b0aec111d5cd57a6cea9ce016442599a615611b8
Songen <957574400@qq.com>
2018-03-22 09:05:29 +0800

    后台页面

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,32 @@ Route::group([],function(){
     //Admin\Users\MangerController
     Route::get('/admin/users/manger/index','Admin\Users\MangerController@index');
     Route::get('/admin/users/manger/list','Admin\Users\MangerController@list');
+    Route::get('/admin/users/manger/add','Admin\Users\MangerController@add');
+    Route::get('/admin/users/manger/del','Admin\Users\MangerController@del');
 
     //后台普通用户路由
+    Route::get('/admin/users/users/list','Admin\Users\UserController@list');
+    Route::get('/admin/users/users/grade','Admin\Users\UserController@grade');
+    Route::get('/admin/users/users/audit','Admin\Users\UserController@audit');
 //    Admin\Users\UserController
     //后台商家路由
+    Route::get('/admin/biz/biz/list','Admin\Biz\MerchantController@list');
+    Route::get('/admin/biz/biz/grade','Admin\Biz\MerchantController@grade');
+    Route::get('/admin/biz/biz/audit','Admin\Biz\MerchantController@audit');
     //Admin\Users\SellerController
     //后台商铺路由
+    Route::get('/admin/shops/shop/list','Admin\Shops\ShopsController@list');
+    Route::get('/admin/shops/shop/add','Admin\Shops\ShopsController@add');
     //Admin\Shops\ShopController
     //后台商品路由
+    Route::get('/admin/goods/good/list','Admin\Goods\GoodsController@list');
+    Route::get('/admin/goods/good/add','Admin\Goods\GoodsController@add');
+    Route::get('/admin/goods/good/del','Admin\Goods\GoodsController@del');
     //Admin\Goods\GoodController
     //后台订单路由
+    Route::get('/admin/order/order/list','Admin\Order\OrdersController@list');
+    Route::get('/admin/order/order/add','Admin\Order\OrdersController@add');
+    Route::get('/admin/order/order/del','Admin\Order\OrdersController@del');
     //Admin\Orders\OrderController
 });
 
