260ae82820b851a6f20fbd6f2d40daf1664332b8
zhangyuchuan <18410100203@163.com>
2018-03-26 19:41:43 +0800

    3.26全部合并

--- app/Http/Controllers/Admin/Users/MangerController.php
+++ app/Http/Controllers/Admin/Users/MangerController.php
@@ -310,40 +310,4 @@ class MangerController extends Controller
 
     }
 
-
-
-    public function  store(Request $request)
-    {
-        //接受用户提交的添加信息
-        $input =$request->all();
-        return $input;
-
-        //表单验证,检查用户是否注册,确认密码是否一致
-
-        //将数据添加到数据库
-        $pass =Crypt::encrypt($input['pass']);
-        $res = User::create(['user_name'=>$input['username'],'user_pass'=>$pass]);
-
-         if($res){
-//            json格式的接口信息  {'status':是否成功，'msg'：提示信息}
-             $arr = [
-                 'status'=>0,
-                 'msg'=>'添加成功'
-             ];
-         }else{
-             $arr = [
-                 'status'=>1,
-                 'msg'=>'添加失败'
-             ];
-         }
-
-//        json_encode($arr);
-//        return response()->json($arr);
-
-        return $arr;
-    }
-
-    //成功跳转
-
-
 }
